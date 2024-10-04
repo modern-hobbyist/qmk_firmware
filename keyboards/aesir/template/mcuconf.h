d1a1258c4aea346462cc5c8c15617b2471cbc2a0
Charlie Steenhagen <csteen1005@gmail.com>
2024-10-04 14:46:56 -0500

    Attempted to fix some things

--- keyboards/aesir/template/mcuconf.h
+++ keyboards/aesir/template/mcuconf.h
@@ -18,3 +18,10 @@
 
 #undef STM32_I2C_USE_I2C1
 #define STM32_I2C_USE_I2C1 TRUE
+
+#undef STM32_SPI_USE_SPI2
+#define STM32_SPI_USE_SPI2 TRUE
+
+// Used for backlight
+#undef STM32_PWM_USE_TIM4
+#define STM32_PWM_USE_TIM4 TRUE
