cee24e34b3f5ab94c6f698cc880ede847d80077e
Charlie Steenhagen <csteen1005@gmail.com>
2025-10-30 11:57:57 -0500

    Fixed config for bootmagic

---
+++ keyboards/aesir/mist/halconf.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// #define HAL_USE_GPT TRUE
+#define HAL_USE_PWM TRUE
+#define HAL_USE_SPI TRUE
+// #define HAL_USE_SERIAL TRUE
+// #define SERIAL_BUFFERS_SIZE 256
+
+#include_next <halconf.h>
