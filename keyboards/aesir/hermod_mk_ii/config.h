0b3d38a1a56e18209e73b882ee8f2d1b2d78c47b
Charlie Steenhagen <csteen1005@gmail.com>
2025-12-28 21:36:23 -0600

    Created new firmware for hermod_mk_ii

---
+++ keyboards/aesir/hermod_mk_ii/config.h
@@ -0,0 +1,74 @@
+/* Copyright 2024 Modern Hobbyist <https://github.com/modern-hobbyist>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+// Bootloader
+#define STM32_BOOTLOADER_DUAL_BANK TRUE
+#define STM32_BOOTLOADER_DUAL_BANK_GPIO B7
+
+#define BOOTMAGIC_ROW_RIGHT 7
+#define BOOTMAGIC_COLUMN_RIGHT 6
+
+// #ifdef RGB_MATRIX_TIMEOUT
+//  #undef RGB_MATRIX_TIMEOUT
+// #endif
+
+#define SPI_DRIVER SPID1
+#define SPI_SCK_PIN A5
+#define SPI_MISO_PIN A7
+#define SPI_MOSI_PIN A6
+#define SPI_SCK_PAL_MODE 5
+#define SPI_MISO_PAL_MODE 5
+#define SPI_MOSI_PAL_MODE 5
+
+#define LCD_RST_PIN B5
+#define LCD_CS_PIN B6
+#define LCD_DC_PIN B4
+#ifndef LCD_ACTIVITY_TIMEOUT
+#    define LCD_ACTIVITY_TIMEOUT 600000
+#endif
+
+// Backlight driver (to control LCD backlight)
+#define BACKLIGHT_PWM_DRIVER PWMD1
+#define BACKLIGHT_PAL_MODE 6
+#define BACKLIGHT_PWM_CHANNEL 1
+#define BACKLIGHT_LEVELS 10
+#define BACKLIGHT_PIN A8
+
+
+#define WS2812_PWM_PAL_MODE 2
+#define WS2812_PWM_DRIVER PWMD8
+#define WS2812_PWM_CHANNEL 1
+#define WS2812_PWM_DMAMUX_ID STM32_DMAMUX1_TIM8_UP
+
+#undef SERIAL_USART_DRIVER
+#define SERIAL_USART_DRIVER SD2
+
+#undef SERIAL_USART_TX_PAL_MODE
+#define SERIAL_USART_TX_PAL_MODE 7
+
+#undef SERIAL_USART_RX_PAL_MODE
+#define SERIAL_USART_RX_PAL_MODE 7
+
+#define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode.
+#define SERIAL_USART_TX_PIN A9     // USART TX pin
+#define SERIAL_USART_RX_PIN A10     // USART RX pin
+
+#undef STM32_ONBOARD_EEPROM_SIZE
+#define STM32_ONBOARD_EEPROM_SIZE 2048
+
+#define RGB_MATRIX_DISABLE_SHARED_KEYCODES
+#define RGB_MATRIX_KEYPRESSES
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
